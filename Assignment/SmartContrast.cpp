ada305a908d900f8fd3641685f1ab6fa00598724
Wouter de Bruijn <wouter@hedium.nl>
2022-04-28 11:14:21 +0200

    :sparkles: Added contrast stretching

--- Assignment/SmartContrast.cpp
+++ Assignment/SmartContrast.cpp
@@ -16,30 +16,57 @@ SmartContrast::SmartContrast(Mat &src, Mat &dst)
 void SmartContrast::process(void)
 {
     int minValue = 255;
-    int avgValueSum = 0;
+    int maxValue = 0;
 
     int h, w, temp = 0;
+
+    int histogram[256] = {0};
+
     for (h = 0; h < HEIGHT; h++)
     {
         for (w = 0; w < WIDTH; w++)
         {
             temp = src.at<uchar>(h, w);
-            if (temp < minValue)
-            {
-                minValue = temp;
-            }
-            avgValueSum = avgValueSum + temp;
+            histogram[temp]++;
         }
     }
 
-    int avgValue = avgValueSum / (HEIGHT * WIDTH);
+    for (int i = 256; i > 0; i--)
+    {
+        if (histogram[i] > 200)
+        {
+            maxValue = i;
+            break;
+        }
+    }
+
+    for (int i = 0; i < 256; i++)
+    {
+        if (histogram[i] > 200)
+        {
+            minValue = i;
+            break;
+        }
+    }
+
+    cout << "minValue: " << minValue << endl;
+    cout << "maxValue: " << maxValue << endl;
 
     for (h = 0; h < HEIGHT; h++)
     {
         for (w = 0; w < WIDTH; w++)
         {
             temp = src.at<uchar>(h, w);
-            dst.at<uchar>(h, w) = (temp - minValue) + (LEVELS / 2 - avgValue); // This needs to be changed to proper contrast stretching
+            int newValue = (float)(temp - minValue) / (maxValue - minValue) * 255;
+
+            // Prevent "clipping"
+            if (newValue > 255)
+                newValue = 255;
+
+            if (newValue < 0)
+                newValue = 0;
+
+            dst.at<uchar>(h, w) = newValue;
         }
     }
 }
